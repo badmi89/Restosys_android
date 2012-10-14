ae92de322f4b25f84c70926d0a7832b2c38d708b
badmi89 <badmi89@gmail.com>
2012-10-14 15:59:59 +0200

    Test

--- gen/com/milos/restosys/R.java
+++ gen/com/milos/restosys/R.java
@@ -11,22 +11,53 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int barcode=0x7f020000;
+        public static final int bglines=0x7f020001;
+        public static final int bground=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int powericon=0x7f020005;
+        public static final int restosys=0x7f020006;
+        public static final int settings=0x7f020007;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int barcodeButton=0x7f07000c;
+        public static final int button1=0x7f070000;
+        public static final int button2=0x7f070008;
+        public static final int buttonLogin=0x7f07000b;
+        public static final int checkBox1=0x7f070005;
+        public static final int checkBox2=0x7f070006;
+        public static final int editTextLogin=0x7f07000a;
+        public static final int imageView1=0x7f070009;
+        public static final int linearLayout1=0x7f070004;
+        public static final int menu_settings=0x7f07000d;
+        public static final int powerOffButton=0x7f070002;
+        public static final int printButton=0x7f070007;
+        public static final int settingButton=0x7f070003;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
+        public static final int activity_bills=0x7f030000;
+        public static final int activity_login=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_login=0x7f060000;
+        public static final int activity_bills=0x7f060000;
+        public static final int activity_login=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int billsLabel=0x7f040009;
+        public static final int comming_soon=0x7f040007;
+        public static final int createNewBill=0x7f040008;
+        public static final int deleteButton=0x7f04000d;
         public static final int hello_world=0x7f040001;
+        public static final int logOffIcon=0x7f04000b;
+        public static final int loginBarcodeButton=0x7f040005;
+        public static final int loginButton=0x7f040004;
         public static final int menu_settings=0x7f040002;
+        public static final int printButton=0x7f04000c;
+        public static final int settingsIcon=0x7f04000a;
+        public static final int title_activity_bills=0x7f040006;
         public static final int title_activity_login=0x7f040003;
     }
     public static final class style {
--- src/com/milos/restosys/LoginActivity.java
+++ src/com/milos/restosys/LoginActivity.java
@@ -2,28 +2,54 @@ package com.milos.restosys;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.backup.BackupManager;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 	
-	BackupManager manager;
+	private Button loginButton;
+	private ImageButton barcodeButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
-        manager = new BackupManager(this);
-        manager.dataChanged();
-        
-        Toast.makeText(this, "Backed up!", Toast.LENGTH_LONG).show();
+        initLoginButton();
+        initBarcodeLoginButton();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_login, menu);
-        return true;
+    private Button initLoginButton() {
+    	if (loginButton == null) {
+    		loginButton = (Button) findViewById(R.id.buttonLogin);
+    		loginButton.setOnTouchListener(new OnTouchListener() {
+				
+				public boolean onTouch(View v, MotionEvent event) {
+					Intent billActivity = new Intent("com.milos.restosys.BILLS_ACTIVITY");
+					startActivity(billActivity);
+					finish();
+					return true;
+				}
+			});
+    	}
+    	return loginButton;
+    }
+    
+    private ImageButton initBarcodeLoginButton() {
+    	if (barcodeButton == null) {
+    		barcodeButton = (ImageButton) findViewById(R.id.barcodeButton);
+    		barcodeButton.setOnTouchListener(new OnTouchListener() {
+				
+				public boolean onTouch(View v, MotionEvent event) {
+					Toast.makeText(getApplicationContext(), R.string.comming_soon, Toast.LENGTH_LONG).show();
+					return true;
+				}
+			});
+    	}
+    	return barcodeButton;
     }
 }
